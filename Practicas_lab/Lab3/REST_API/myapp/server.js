a7feb7a450a7b003ca1fef233d81a78e78362eb5
Arnau <arnaugg9@gmail.com>
2025-09-27 14:10:09 +0200

    Finiched Lab3 intro

--- Practicas_lab/Lab3/REST_API/myapp/server.js
+++ Practicas_lab/Lab3/REST_API/myapp/server.js
@@ -6,14 +6,30 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`PTI HTTP Server listening at http://localhost:${port}`)
-})
-
 app.get("/students", (req, res, next) => {
- res.send('Received request at /students')
+    res.json({
+        responseId: 1234,
+        students: [
+            {name: "Jordi", studentId: '12345678a'},
+            {name: "Marta", studentId: '12345678b'}
+    ]});
 });
+
 
 app.get('/students/:studentId', function (req, res) {
     res.send('Received request at /students with param studentId='+req.params.studentId)
+})
+
+app.use(express.json());
+
+app.post('/newstudent', (req, res, next) => {
+    for(var i in req.body.students){
+        console.log(req.body.students[i].name+'\n');
+    }
+    res.status(201);
+    res.end(); 
+}) 
+
+app.listen(port, () => {
+  console.log(`PTI HTTP Server listening at http://localhost:${port}`)
 })
